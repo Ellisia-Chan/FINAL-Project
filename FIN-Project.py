467a33f55d1e57536cbbcf40abf1ac830b72679f
ChristianJude23 <152279955+ChristianJude23@users.noreply.github.com>
2023-12-06 13:13:32 +0800

    FIN-Project.py
    v.1.2

--- FIN-Project.py
+++ FIN-Project.py
@@ -1,5 +1,5 @@
 def generate_table(num_items, base):
-    print(f"{'Item':<15}{'Base':<15}")
+    print(f"{'Item':<12}| {'Base':<15}")
     print('-' * 30)
 
     if num_items > 0:
@@ -10,12 +10,12 @@ def generate_table(num_items, base):
 
     for i in range(num_items, -1, -1):
         item = f"Score {i}"
-        print(f"{item:<15}{base:<15}")
+        print(f"{item:<12}|  {base:<15,.2f}")
 
         # Decrement the base value by the calculated increment
         base -= base_increment
 
 items = int(input("Enter the number of items: "))
-base = int(input("Enter the base value: "))
+base = float(input("Enter the base value: "))
 
 generate_table(items, base)
